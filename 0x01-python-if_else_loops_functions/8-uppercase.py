20c745c3e661031135e4c26500bae11454adaa1f
Secatricia <3560@holbertonschool.com>
2021-12-21 22:57:38 +0000

    Correction check exo 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    i = 0
-    ch2 = ''
-    while str[i:]:
-        ch = ord(str[i])
-        if ch > 96 and ch < 123:
-            ch2 = chr(ch-32)
-            print(ch2, end="")
-        else:
-            print(str[i], end="")
-        i += 1
-    print("")
+    for c in str:
+        if ord('a') <= ord(c) <= ord('z'):
+            c = chr(ord(c) - 32)
+        print("{:s}".format(c), end='')
+    print()
