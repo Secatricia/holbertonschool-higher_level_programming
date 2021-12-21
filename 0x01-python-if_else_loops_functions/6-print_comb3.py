51da0a8dc6a4f7607fcd8fbc52a9ae168c4e9711
Secatricia <3560@holbertonschool.com>
2021-12-21 22:52:19 +0000

    Correction check exo 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 8):
-    for j in range(0, 10):
-        if (i < j):
-            print(i, j, ", ", sep="", end="")
-print("89")
+for i in range(0, 10):
+    for j in range(i + 1, 10):
+        if (i == 8 and j == 9):
+            print("{:d}{:d}".format(i, j))
+        else:
+            print("{:d}{:d}, ".format(i, j), end='')
