3a5346cf4301accc75a83734cd40065c91c51492
Secatricia <3560@holbertonschool.com>
2021-12-22 09:11:47 +0000

    Correction exo 3 lowercase

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(65, 91):
-    if i == 69 or i == 81:
+for i in range(97, 123):
+    if i == 101 or i == 113:
         continue
     else:
         print("{:c}".format(i), end='')
