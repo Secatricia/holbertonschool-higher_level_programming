372e0d4de966dd3b91a615a39192726f851998b5
Secatricia <3560@holbertonschool.com>
2021-12-22 11:47:05 +0000

    Correction requirement exo 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-i = 122
-while i != 96:
+for i in range(97, 123)[::-1]:
     if (i % 2) != 0:
         i -= 32
     print(chr(i), end="")
     if i > 64 and i < 91:
         i += 32
-    i -= 1
