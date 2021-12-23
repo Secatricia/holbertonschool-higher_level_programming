91a8b56fca620a3ea972bb434ab3b27e55efc1c1
Secatricia <3560@holbertonschool.com>
2021-12-23 11:10:07 +0000

    Suppr space exo 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 import calculator_1 as f
 a = 10
 b = 5
-print("{:d} + {:d} = ".format(a, b), f.add(a, b))
-print("{:d} - {:d} = ".format(a, b), f.sub(a, b))
-print("{:d} * {:d} = ".format(a, b), f.mul(a, b))
-print("{:d} / {:d} = ".format(a, b), f.div(a, b))
+print("{:d} + {:d} =".format(a, b), f.add(a, b))
+print("{:d} - {:d} =".format(a, b), f.sub(a, b))
+print("{:d} * {:d} =".format(a, b), f.mul(a, b))
+print("{:d} / {:d} =".format(a, b), f.div(a, b))
