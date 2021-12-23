d9b1384ba04e08c2fadc7456e4000af87a313af0
Secatricia <3560@holbertonschool.com>
2021-12-23 12:54:57 +0000

    Correction exo 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import calculator_1 as f
+from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
-print("{:d} + {:d} = {} ".format(a, b, f.add(a, b)))
-print("{:d} - {:d} = {}".format(a, b, f.sub(a, b)))
-print("{:d} * {:d} = {}".format(a, b, f.mul(a, b)))
-print("{:d} / {:d} = {}".format(a, b, f.div(a, b)))
+if __name__ == '__main__':
+    print("{:d} + {:d} = {:d} ".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
