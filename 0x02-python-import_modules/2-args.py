737b86aa5c4c2df31ee0b0919cbe13346a4fdc44
Secatricia <3560@holbertonschool.com>
2021-12-23 15:00:37 +0000

    Add __name__

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,13 @@
 from sys import argv
 i = 0
 j = len(argv) - 1
-if j == 0:
-    print("{} arguments.".format(j))
-elif j == 1:
-    print("{} argument:".format(j))
-else:
-    print("{} arguments:".format(j))
-while (i < j):
-    i += 1
-    print("{}: {}".format(i, argv[i]))
+if __name__ == '__main__':
+    if j == 0:
+        print("{} arguments.".format(j))
+    elif j == 1:
+        print("{} argument:".format(j))
+    else:
+        print("{} arguments:".format(j))
+    while (i < j):
+        i += 1
+        print("{}: {}".format(i, argv[i]))
