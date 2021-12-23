ce8f86a9480cb722c6f800e6a63e2e2431555438
Secatricia <3560@holbertonschool.com>
2021-12-23 11:06:34 +0000

    Add exo 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import calculator_1 as f
+a = 10
+b = 5
+print("{:d} + {:d} = ".format(a, b), f.add(a, b))
+print("{:d} - {:d} = ".format(a, b), f.sub(a, b))
+print("{:d} * {:d} = ".format(a, b), f.mul(a, b))
+print("{:d} / {:d} = ".format(a, b), f.div(a, b))
