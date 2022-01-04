14565e3aeb2a2f2f53c69054a586e27f40ee03b1
Secatricia <3560@holbertonschool.com>
2022-01-04 15:13:02 +0000

    Add condition if sentence = Null ex 7

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if(len(sentence) == 0):
-        return None
     lens = len(sentence)
+    if(len(sentence) == 0):
+        return(0, None)
     firstc = sentence[0]
     result = lens, firstc
-    return result
- 
+    return(result)
