13e3627faff6009a4599eed501ef308dd1e0b688
Secatricia <3560@holbertonschool.com>
2022-01-04 10:02:26 +0000

    Requirement exo 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             print("{}".format(matrix[i][j]), end="")
-            if j + 1 < len(matrix[i]):
+            if (j + 1 < len(matrix[i])):
                 print(" ", end="")
         print()
