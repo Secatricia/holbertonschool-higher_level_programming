2239af93cf0e7b7c273fd2fbeb90fbf2f19d56c0
Secatricia <3560@holbertonschool.com>
2022-01-04 13:37:01 +0000

    Correction ex 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,5 +5,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
             tuple_b = tuple_b + (0, 0)
         else:
             tuple_a = tuple_a + (0, 0)
-    new = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
+        new = tuple(map(sum, zip(tuple_a, tuple_b)))
+    else:
+        new = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
     return (new)
