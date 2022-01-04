1367a3679b6c117a58e84a4585fd5fa094d0bd88
Secatricia <3560@holbertonschool.com>
2022-01-04 15:03:21 +0000

    Add ex 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    if(len(sentence) == 0):
+        return None
     lens = len(sentence)
     firstc = sentence[0]
     result = lens, firstc
     return result
+ 
