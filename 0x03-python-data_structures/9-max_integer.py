1bc1893554a16798cc1e2e620387556c8d92ee52
Secatricia <3560@holbertonschool.com>
2022-01-04 15:40:31 +0000

    Correction ex 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@ from typing import Sequence
 
 def max_integer(my_list=[]):
     res = 0
-    if( len(my_list) == 0):
+    if(len(my_list) == 0):
         return None
     for i in range(len(my_list)):
         if (my_list[i] > res):
