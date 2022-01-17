00674f0e65a8c64b468b0201f877172c1aabba2b
Secatricia <3560@holbertonschool.com>
2022-01-17 16:43:06 +0000

    Add var index

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    index = 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-            i += 1
+            index += 1
         except IndexError:
             break
     print()
-    return(i)
+    return(index)
