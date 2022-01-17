b2ae15086e69cdb0e3a99b92047a347cdb35d1a5
Secatricia <3560@holbertonschool.com>
2022-01-17 16:45:47 +0000

    Delete import

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-from ast import Return
-from pickle import FALSE, TRUE
-
 
 def safe_print_integer(value):
     try:
         if type(value) == int:
             print("{}".format(value))
-            return(True)
+            return (True)
     except ValueError:
-        Return(False)
+        return (False)
