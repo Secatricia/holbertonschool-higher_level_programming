3f6a803e45b4647f173a004f345ccb9093be21c2
Secatricia <3560@holbertonschool.com>
2022-01-24 11:00:40 +0000

    Add ex 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Create class rectangle"""
+
+
+class Rectangle:
+    """Initialisation width height"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    """Define width"""
+    @property
+    def width(self):
+        """Recup width"""
+        return (self.__width)
+
+    """Define width"""
+    @width.setter
+    def width(self, value):
+        """Test Error"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    """Define height"""
+    @property
+    def height(self):
+        """Recup height"""
+        return(self.__height)
+
+    """Define height"""
+    @height.setter
+    def height(self, value):
+        """Test Error"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
