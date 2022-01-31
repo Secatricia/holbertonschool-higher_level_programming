856a953cf2b33b05ddf641d7704dd17123b18fb6
Secatricia <3560@holbertonschool.com>
2022-01-31 17:23:31 +0000

    Requirement ex 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -3,13 +3,18 @@
 (9-rectangle.py)"""
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square:
+    """Create class that define Square"""
+
     def __init__(self, size):
         """Create Rectangle class"""
         self.__size = size
 
     def area(self):
+        """Function that return square area"""
         return (self.__size * self.__size)
 
     def __str__(self):
+        """print info"""
         return ("[Rectangle] {}/{}".format(self.__size, self.__size))
