65a8caab179b4aad16168ed3aad92df414b68b25
Secatricia <3560@holbertonschool.com>
2022-01-31 16:02:09 +0000

    Add tests 1

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -18,10 +18,14 @@ class BaseGeometry:
         if value <= 0:
             raise TypeError("{} must be greater than 0".format(name))
 
-"""class Rectangle that inherits
-from BaseGeometry (7-base_geometry.py)"""
+
+"""class Rectangle that inherits from
+BaseGeometry (7-base_geometry.py)"""
+
+
 class Rectangle(BaseGeometry):
     """Initialise width and height"""
+
     def __init__(self, width, height):
         """Call integer_validator"""
         super().integer_validator("width", width)
