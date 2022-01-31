7c349b7099682cd885514ed99e7aabb5b5957c52
Secatricia <3560@holbertonschool.com>
2022-01-31 16:15:20 +0000

    Add test 7

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """class BaseGeometry (based on 5-base_geometry.py)"""
+
+
 class BaseGeometry:
     """Create BaseGeometry class"""
+
     def area(self):
+        """Raise Error"""
         raise Exception("area() is not implemented")
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,23 +1,5 @@
 #!/usr/bin/python3
-"""class BaseGeometry (based on 5-base_geometry.py)"""
-
-
-class BaseGeometry:
-    """Create BaseGeometry class"""
-
-    def area(self):
-        """Raise Error"""
-        raise Exception("area() is not implemented")
-
-    """Create function that raise Error"""
-
-    def integer_validator(self, name, value):
-        """Test with value"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise TypeError("{} must be greater than 0".format(name))
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 """class Rectangle that inherits from
 BaseGeometry (7-base_geometry.py)"""
