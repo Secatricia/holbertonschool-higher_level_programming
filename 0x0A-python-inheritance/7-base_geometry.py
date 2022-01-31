5c2f4743a67495d02057730f4f456007015191e1
Secatricia <3560@holbertonschool.com>
2022-01-31 13:24:57 +0000

    Add ex 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""class BaseGeometry (based on 5-base_geometry.py)"""
+
+
+class BaseGeometry:
+    """Create BaseGeometry class"""
+
+    def area(self):
+        """Raise Error"""
+        raise Exception("area() is not implemented")
+
+    """Create function that raise Error"""
+    def integer_validator(self, name, value):
+        """Test with value"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise TypeError("{} must be greater than 0".format(name))
