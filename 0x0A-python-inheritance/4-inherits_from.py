645f7824817954d65535ff34290596890a2ea715
Secatricia <3560@holbertonschool.com>
2022-01-31 17:09:35 +0000

    Add requirement ex 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,6 +2,8 @@
 """function that returns True if the object is an instance
 of a class that inherited (directly or indirectly)
 from the specified class ; otherwise False."""
+
+
 def inherits_from(obj, a_class):
     """Return True or False"""
     if type(obj) != a_class:
