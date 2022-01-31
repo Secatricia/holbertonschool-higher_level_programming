17ea779c97afedd35f9857c7e7a8c8267f505fd4
Secatricia <3560@holbertonschool.com>
2022-01-31 12:24:55 +0000

    Add ex 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""function that returns True if the object is an instance
+of a class that inherited (directly or indirectly)
+from the specified class ; otherwise False."""
+def inherits_from(obj, a_class):
+    """Return True or False"""
+    if type(obj) != a_class:
+        return True
+    else:
+        return False
