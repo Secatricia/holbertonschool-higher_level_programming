06d6205c2ef98d81627b67d8e687726404747a93
Secatricia <3560@holbertonschool.com>
2022-02-01 15:56:45 +0000

    Add ex 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -3,6 +3,7 @@ import json
 """function that returns the JSON
 representation of an object (string):"""
 
+
 def to_json_string(my_obj):
     """define function to_json_string"""
     return json.dumps(my_obj)
