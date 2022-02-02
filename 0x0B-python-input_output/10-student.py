4c968e5a64d77269fa856b792ff918b7f1362a3f
Secatricia <3560@holbertonschool.com>
2022-02-02 20:30:05 +0000

    Add ex 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" class Student that defines a student by:
+(based on 9-student.py)"""
+
+
+class Student:
+    """init class"""
+
+    def __init__(self, first_name, last_name, age):
+        """define init function"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """define to_json function"""
+        if type(attrs) is list:
+            return {i: self.__dict__.get(i) for i in attrs
+                    if i in self.__dict__}
+        return vars(self)
