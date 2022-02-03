4359ff2a37c0d1cfa0fd062d320d79ab302fffb0
Secatricia <3560@holbertonschool.com>
2022-02-03 15:50:03 +0000

    Add ex 3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -68,3 +68,7 @@ class Rectangle(Base):
         if y < 0:
             raise ValueError("y must be >= 0")
         self.__y = y
+
+    def area(self):
+        """Define area function"""
+        return self.height * self.width
