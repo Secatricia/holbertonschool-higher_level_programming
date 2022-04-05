b043acb656b026495c7700be5aa12dcc5969081c
Secatricia <3560@holbertonschool.com>
2022-04-05 16:34:36 +0000

    Change semistandard 4,5,6

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -9,12 +9,12 @@ class Rectangle {
   }
 
   print () {
-	for (let i = 0; i < this.height; i++) {
-		for (let i = 0; i < this.width; i++) {
-		  process.stdout.write('X');
-		}
-		console.log('');
-	  }
+    for (let i = 0; i < this.height; i++) {
+      for (let i = 0; i < this.width; i++) {
+        process.stdout.write('X');
+      }
+      console.log('');
+    }
   }
 
   rotate () {
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,33 +1,33 @@
 #!/usr/bin/node
 
 class Rectangle {
-	constructor (w, h) {
-	  if (w > 0 && h > 0) {
-		this.width = w;
-		this.height = h;
-	  }
-	}
-  
-	print () {
-	  for (let i = 0; i < this.height; i++) {
-		  for (let i = 0; i < this.width; i++) {
-			process.stdout.write('X');
-		  }
-		  console.log('');
-		}
-	}
-  
-	rotate () {
-	  const cpheight = this.height;
-	  this.height = this.width;
-	  this.width = cpheight;
-	}
-  
-	double () {
-	  this.width = this.width * 2;
-	  this.height = this.height * 2;
-	}
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
   }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let i = 0; i < this.width; i++) {
+        process.stdout.write('X');
+      }
+      console.log('');
+    }
+  }
+
+  rotate () {
+    const cpheight = this.height;
+    this.height = this.width;
+    this.width = cpheight;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
 
 class Square extends Rectangle {
   constructor (size) {
