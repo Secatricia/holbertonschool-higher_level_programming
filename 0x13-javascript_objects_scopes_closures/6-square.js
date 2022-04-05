be2eec7a9a99ea5292a10990e7d7598df0c48a26
Secatricia <3560@holbertonschool.com>
2022-04-05 16:47:35 +0000

    Add semi 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,33 +1,6 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      for (let i = 0; i < this.width; i++) {
-        process.stdout.write('X');
-      }
-      console.log('');
-    }
-  }
-
-  rotate () {
-    const cpheight = this.height;
-    this.height = this.width;
-    this.width = cpheight;
-  }
-
-  double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
-  }
-}
+const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
@@ -47,6 +20,4 @@ class Square extends Rectangle {
     }
   }
 }
-
-module.exports = Rectangle;
 module.exports = Square;
