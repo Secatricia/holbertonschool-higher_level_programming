c27e150d782c45b8ccf89d05d1480e3466e4ef11
Secatricia <3560@holbertonschool.com>
2022-04-06 10:38:22 +0000

    Change condition ex 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,21 +1,21 @@
 #!/usr/bin/node
 
 let secondBig = 0;
-let firstBig = process.argv[2];
+let firstBig = parseInt(process.argv[2]);
 const argvlist = process.argv;
 let j = 2;
 let i = 2;
 
 if (argvlist.length > 3) {
   for (j in argvlist) {
-    if (argvlist[j] > firstBig) {
-      firstBig = argvlist[j];
+    if (parseInt(argvlist[j]) > firstBig) {
+      firstBig = parseInt(argvlist[j]);
     }
     j++;
   }
   for (i in process.argv) {
     if (process.argv[i] > secondBig && process.argv[i] < firstBig) {
-      secondBig = process.argv[i];
+      secondBig = parseInt(process.argv[i]);
     }
     i++;
   }
