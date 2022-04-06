ab00ec10685ee9c69f4a311e2f07bed84b72f6b9
Secatricia <3560@holbertonschool.com>
2022-04-06 09:49:23 +0000

    Add ex 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,25 @@
 #!/usr/bin/node
 
+let secondBig = 0;
+let firstBig = 0;
+
 if (process.argv[2] === undefined || process.argv[3] === undefined) {
-	console.log('0');
-}
-else {
-	for (i in process.argv) {
-		secondBig = 0;
-	}
+  console.log('0');
+} else {
+  for (const j in process.argv) {
+    if (process.argv[j] > firstBig) {
+      firstBig = process.argv[j];
+    }
+  }
+  for (let i in process.argv) {
+    if (process.argv[i] > secondBig) {
+      if (process.argv[i] < firstBig) {
+        secondBig = process.argv[i];
+      } else {
+        firstBig = process.argv[i];
+      }
+    }
+    i++;
+  }
+  console.log(secondBig);
 }
