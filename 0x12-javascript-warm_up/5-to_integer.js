587492330e46a84d6244e1086ae431b3a045df3b
Secatricia <3560@holbertonschool.com>
2022-04-06 07:49:33 +0000

    Change space if argv is an integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 if (parseInt(process.argv[2])) {
-  console.log('My number: ', parseInt(process.argv[2]));
+  console.log('My number:', parseInt(process.argv[2]));
 } else if (parseInt(process.argv[2]) === 0) {
   console.log('My number: 0');
 } else {
