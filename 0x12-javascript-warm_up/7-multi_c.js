ca0baa00baa4c188417808436fa1b8d6d6d591c3
Secatricia <3560@holbertonschool.com>
2022-04-06 08:19:32 +0000

    Change Number

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (parseInt(process.argv[2]) && process.argv[2] > 0) {
+if (Number(process.argv[2]) && process.argv[2] > 0) {
   while (process.argv[2] > 0) {
     console.log('C is fun');
     process.argv[2] -= 1;
