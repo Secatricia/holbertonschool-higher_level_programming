f6ca96eb5693838d38d6d0fd1eea21636bfbf6cf
Secatricia <3560@holbertonschool.com>
2022-04-22 09:02:22 +0000

    Pycodestyle 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # Python script that fetches https://intranet.hbtn.io/status
 import urllib.request
+
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         data = response.read()
