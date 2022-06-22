c7de70a426786b092c2cc93fc231712f4747e5ba
Secatricia <3560@holbertonschool.com>
2022-06-22 13:55:04 +0000

    Correct 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,16 +2,11 @@
 """takes in a URL and an email, sends a POST request
 to the passed URL with the email as a parameter"""
 
-from requests import request
-
-
 if __name__ == "__main__":
     import urllib.request
     from sys import argv
 
-    val = {'email': argv[2]}
-    content = urllib.parse.urlencode(val).encode('ascii')
-    req = urllib.request.Request(argv[1], data=content, method='POST')
-    with urllib.request.urlopen(req) as n:
-        readed = n.read().decode('utf-8')
-        print(readed)
+    with urllib.request.urlopen(argv[1]) as r:
+        name = r.name()
+        content = name.get('X-Request-Id')
+        print(content)
